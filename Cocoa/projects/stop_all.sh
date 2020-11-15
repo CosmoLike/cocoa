6539b68af1efe464e9c460a92f79f50d34b0b326
Vivian Miranda <vivianmiranda@arizona.edu>
2020-11-14 23:18:24 -0700

    update

---
+++ Cocoa/projects/stop_all.sh
@@ -0,0 +1,15 @@
+cd $ROOTDIR/projects
+
+for NAME in $(find . -mindepth 1 -maxdepth 1 -type d ! -name 'example'); do
+  cd $ROOTDIR/projects/$NAME/scripts
+  # https://stackoverflow.com/a/11231970/2472169
+  # we don't want the sh to crash if source
+  FNAME = stop_$NAME
+  if [ -f "$FNAME" ]; then
+    source $FNAME
+  fi
+done
+
+source delete_link.sh
+
+cd $ROOTDIR
