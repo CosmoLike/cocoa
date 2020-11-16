af47bba686ea25ccb58987f10c7f991c2136954b
Vivian Miranda <vivianmiranda@arizona.edu>
2020-11-16 13:41:27 -0700

    update

--- Cocoa/projects/stop_all.sh
+++ Cocoa/projects/stop_all.sh
@@ -4,10 +4,11 @@ for NAME in $(find . -mindepth 1 -maxdepth 1 -type d ! -name 'example'); do
   cd $ROOTDIR/projects/$NAME/scripts
   # https://stackoverflow.com/a/11231970/2472169
   # we don't want the sh to crash if source
-  FNAME = stop_$NAME
+  declare FNAME=stop_$NAME
   if [ -f "$FNAME" ]; then
     source $FNAME
   fi
+  cd $ROOTDIR/projects
 done
 
 rm -rf $ROOTDIR/projects/.gitignore
