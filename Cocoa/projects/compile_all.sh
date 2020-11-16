f2540188fd3712dba18db8be90d2b61ebe02fc9f
Vivian Miranda <vivianmiranda@arizona.edu>
2020-11-16 08:23:32 -0700

    update

--- Cocoa/projects/compile_all.sh
+++ Cocoa/projects/compile_all.sh
@@ -4,10 +4,13 @@ for NAME in $(find . -mindepth 1 -maxdepth 1 -type d ! -name 'example'); do
   cd $ROOTDIR/projects/$NAME/scripts
   # https://stackoverflow.com/a/11231970/2472169
   # we don't want the sh to crash if source
-  FNAME = setup_$NAME
+  declare FNAME=setup_$(echo "${NAME}" | sed -E 's@./@@')
+  echo $FNAME
   if [ -f "$FNAME" ]; then
     source $FNAME
   fi
 done
+
+unset $FNAME
 
 cd $ROOTDIR
