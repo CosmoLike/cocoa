ed59ad6a83928e8a6db8efbd9d81c821cb068f19
Vivian Miranda <vivianmiranda@arizona.edu>
2020-11-16 13:41:27 -0700

    update

--- Cocoa/projects/start_all.sh
+++ Cocoa/projects/start_all.sh
@@ -1,22 +1,21 @@
 cd $ROOTDIR/projects
 
-for NAME in $(find . -mindepth 1 -maxdepth 1 -type d ! -name 'example'); do
-  cd $ROOTDIR/projects/$NAME/scripts
-  # https://stackoverflow.com/a/11231970/2472169
-  # we don't want the sh to crash if source
-  FNAME = start_$NAME
-  if [ -f "$FNAME" ]; then
-    source $FNAME
-  fi
-done
-
 # create tmp .gitignore and include all projects in it
 touch $ROOTDIR/projects/.gitignore
 
 for NAME in $(find . -mindepth 1 -maxdepth 1 -type d ! -name 'example'); do
-    echo "${NAME}/" >> .gitignore
+  cd $ROOTDIR/projects/$NAME/scripts
+  # https://stackoverflow.com/a/11231970/2472169
+  # we don't want the sh to crash if source
+  echo $NAME
+  declare FNAME=start_$NAME
+  if [ -f "$FNAME" ]; then
+    source $FNAME
+  fi
+  cd $ROOTDIR/projects
+  echo "${NAME}/" >> $ROOTDIR/projects/.gitignore
 done
 
-source create_link.sh
+source $ROOTDIR/projects/create_link.sh
 
 cd $ROOTDIR
