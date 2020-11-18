69ea884cde27d495c16898b31eab0144231a76cb
Vivian Miranda <vivianmiranda@arizona.edu>
2020-11-17 18:15:40 -0700

    update

--- Cocoa/projects/create_link.sh
+++ Cocoa/projects/create_link.sh
@@ -1,14 +1,16 @@
 cd $ROOTDIR/projects
 
 for NAME in $(find . -mindepth 1 -maxdepth 1 -type d ! -name 'example'); do
+  declare NAME2=$(echo "${NAME}" | sed -E 's@./@@')
+
   ln -s $ROOTDIR/projects/$NAME/interface $ROOTDIR/external_modules/code/
-  mv $ROOTDIR/external_modules/code/interface $ROOTDIR/external_modules/code/$NAME
+  mv $ROOTDIR/external_modules/code/interface $ROOTDIR/external_modules/code/$NAME2
 
   ln -s $ROOTDIR/projects/$NAME/likelihood $ROOTDIR/cobaya/cobaya/likelihoods/
-  mv $ROOTDIR/cobaya/cobaya/likelihoods/likelihood $ROOTDIR/cobaya/cobaya/likelihoods/$NAME
+  mv $ROOTDIR/cobaya/cobaya/likelihoods/likelihood $ROOTDIR/cobaya/cobaya/likelihoods/$NAME2
 
   ln -s $ROOTDIR/projects/$NAME/data $ROOTDIR/external_modules/data/
-  mv $ROOTDIR/external_modules/data/data $ROOTDIR/external_modules/data/$NAME
+  mv $ROOTDIR/external_modules/data/data $ROOTDIR/external_modules/data/$NAME2
 done
 
 cd $ROOTDIR
--- Cocoa/projects/delete_link.sh
+++ Cocoa/projects/delete_link.sh
@@ -1,11 +1,13 @@
 cd $ROOTDIR/projects
 
 for NAME in $(find . -mindepth 1 -maxdepth 1 -type d ! -name 'example'); do
-  rm $ROOTDIR/external_modules/code/$NAME
+  declare NAME2=$(echo "${NAME}" | sed -E 's@./@@')
 
-  rm $ROOTDIR/cobaya/cobaya/likelihoods/$NAME
+  rm -f $ROOTDIR/external_modules/code/$NAME2
 
-  rm $ROOTDIR/external_modules/data/$NAME
+  rm -f $ROOTDIR/cobaya/cobaya/likelihoods/$NAME2
+
+  rm -f $ROOTDIR/external_modules/data/$NAME2
 done
 
 cd $ROOTDIR
