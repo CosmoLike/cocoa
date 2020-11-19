48e665ab551dbe924ce816439b187446b1bf7be3
Vivian Miranda <vivianmiranda@arizona.edu>
2020-11-18 21:14:04 -0700

    updatE

--- Cocoa/projects/.compile_all.sh
+++ Cocoa/projects/.compile_all.sh
@@ -11,9 +11,10 @@ for NAME in $(find . -mindepth 1 -maxdepth 1 -type d ! -name 'example'); do
     echo "Error: directory $ROOTDIR/projects/$NAME2/scripts DOES NOT exists."
   else
     cd $ROOTDIR/projects/$NAME2/scripts
+    echo $ROOTDIR/projects/$NAME2
     # https://stackoverflow.com/a/11231970/2472169
     # we don't want the sh to crash if source
-    declare FNAME=compile_$ NAME2
+    declare FNAME=compile_$NAME2
     if [ -f "$FNAME" ]; then
       source $FNAME
     fi
