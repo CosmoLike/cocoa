3e291d37eca483209d0b33f2447d0107e51f4cfe
Vivian Miranda <vivianmiranda@email.arizona.edu>
2020-11-23 02:51:43 -0800

    update

---
+++ Cocoa/projects/.git_commit.sh
@@ -0,0 +1,21 @@
+if [ -z "${ROOTDIR}" ]; then
+    echo 'ERROR ROOTDIR not define'
+    return
+fi
+
+cd $ROOTDIR/projects
+
+for NAME in $(find . -mindepth 1 -maxdepth 1 -type d ! -name 'example'); do
+  declare NAME2=$(echo "${NAME}" | sed -E 's@./@@')
+  if [ ! -d "$ROOTDIR/projects/$NAME2/" ]; then
+    echo "Error: directory $ROOTDIR/projects/$NAME2/ DOES NOT exists."
+  else
+    cd $ROOTDIR/projects/$NAME2/
+    git commit -am "Update"
+  fi
+done
+
+unset NAME2
+unset FNAME
+
+cd $ROOTDIR
