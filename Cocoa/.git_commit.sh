2c4242695b111180a8bc8078d26c2e292bb7965e
Vivian Miranda <vivianmiranda@arizona.edu>
2020-12-14 22:14:38 -0700

    update

--- Cocoa/.git_commit.sh
+++ Cocoa/.git_commit.sh
@@ -3,5 +3,12 @@ if [ -z "${ROOTDIR}" ]; then
     return
 fi
 
+git update-index --assume-unchanged set_installation_options
+git update-index --assume-unchanged setup_cocoa_installation_packages
+git update-index --assume-unchanged compile_external_modules
+git update-index --assume-unchanged start_cocoa
+git update-index --assume-unchanged stop_cocoa
+git update-index --assume-unchanged clean_all
+
 git commit -am "update"
 source $ROOTDIR/projects/.git_commit.sh
