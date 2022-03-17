d6db2ca86d9d688b5dca7f9c4a8495d5dba6d159
Vivian Miranda <vivianmiranda@arizona.edu>
2022-03-17 00:35:01 -0700

    many updates from cluster branch tested on desy1xcmb

---
+++ Cocoa/external_modules/code/cosmolike/tinker_emulator.h
@@ -0,0 +1,14 @@
+#ifndef __TINKER_EMULATOR_H
+#define __TINKER_EMULATOR_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+double tinker_emulator_massfunc(const double m, const double a); // Neff hardcoded
+
+double tinker_emulator_B1(const double m, const double a);
+
+#ifdef __cplusplus
+}
+#endif
+#endif // HEADER GUARD
