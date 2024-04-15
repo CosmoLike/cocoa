fdad03335b9abe96889d4d560eeddbeb6e803045
JiachuanXu <jiachuanxu@email.arizona.edu>
2024-04-15 02:14:51 -0700

    add new baryon modules called baryons_JX.c/h. Will be moved to baryons.c/h once been tested

---
+++ Cocoa/external_modules/code/cosmolike/baryons_JX.h
@@ -0,0 +1,12 @@
+#ifndef __COSMOLIKE_PARAMETERS_BARYONS_H
+#define __COSMOLIKE_PARAMETERS_BARYONS_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void init_baryons(const char* scenario, int sim_id);
+
+#ifdef __cplusplus
+}
+#endif
+#endif // HEADER GUARD
