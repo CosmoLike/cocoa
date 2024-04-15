b9db1ae3849912db91a8465794ac196506c1f50f
JiachuanXu <jiachuanxu@email.arizona.edu>
2024-04-15 14:27:42 -0700

    updates

--- Cocoa/external_modules/code/cosmolike/baryons_JX.c
+++ Cocoa/external_modules/code/cosmolike/baryons_JX.c
@@ -323,20 +323,3 @@ void init_baryons(const char* scenario, int sim_id)
   }
   set_bary_parameters_to_scenario(scenario, filename, sim_id);
 }
-
-// Test HDF5 reading routine
-int main(){
-  init_baryons("TNG100", 1);
-  init_baryons("HzAGN", 1);
-  init_baryons("mb2", 1);
-  init_baryons("illustris", 1);
-  init_baryons("eagle", 1);
-  init_baryons("cowls_AGN", 1);
-  init_baryons("cowls_AGN", 2);
-  init_baryons("cowls_AGN", 3);
-  init_baryons("BAHAMAS", 1);
-  init_baryons("BAHAMAS", 2);
-  init_baryons("BAHAMAS", 3);
-  for (int i = 1; i< 401; i++) init_baryons("antilles", i);
-  log_info("test_baryon: Done!");
-}
