458caf0628065b89421cb4a4824309dede55a1ce
Vivian <vivian.miranda.astro@gmail.com>
2024-04-24 20:51:58 -0400

    Update

--- cocoa_installation_libraries/get_installation_libraries.sh
+++ cocoa_installation_libraries/get_installation_libraries.sh
@@ -55,4 +55,30 @@ if [ -z "${IGNORE_C_CFITSIO_INSTALLATION}" ]; then
     echo -e '\033[0;32m'"\t\tGETTING CFITSIO LIBRARY DONE"'\033[0m'
 fi
 
+if [ -z "${IGNORE_C_FFTW_INSTALLATION}" ]; then
+    echo -e '\033[0;32m'"\t\tGETTING FFTW LIBRARY"'\033[0m'
+    
+    wget -q http://www.fftw.org/fftw-3.3.10.tar.gz 
+    if [ $? -neq 0 ];then
+        echo -e '\033[0;31m'"FFTW COULD NOT RUN \e[3mWGET"'\033[0m'
+        cd $ROOTDIR
+        return 1
+    fi
+    tar zxvf fftw-3.3.10.tar.gz
+    if [ $? -neq 0 ];then
+        echo -e '\033[0;31m'"FFTW COULD NOT RUN \e[3mTAR"'\033[0m'
+        cd $ROOTDIR
+        return 1
+    fi
 
+    rm -f fftw-3.3.10.tar.gz
+
+    tar -cf - fftw-3.3.10/ | xz -k -1 --threads=$MAKE_NUM_THREADS -c - > fftw.xz
+    if [ $? -neq 0 ];then
+        echo -e '\033[0;31m'"FFTW COULD NOT COMPRESS \e[3mFFTW FOLDER"'\033[0m'
+        cd $ROOTDIR
+        return 1
+    fi
+
+    echo -e '\033[0;32m'"\t\tGETTING FFTW LIBRARY DONE"'\033[0m'
+fi
