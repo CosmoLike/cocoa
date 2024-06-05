fe876079cc4d4a3439de76e69dbd268afdd18015
Vivian Miranda <vivian.miranda@stonybrook.edu>
2024-06-04 23:02:22 -0400

    Update

--- cocoa_installation_libraries/cobaya_changes/cobaya/change_python_files.sh
+++ cocoa_installation_libraries/cobaya_changes/cobaya/change_python_files.sh
@@ -2,8 +2,6 @@
 # ---------------------------------------------------
 # ---------------------------------------------------
 # ---------------------------------------------------
-PLANCK_CLIK='conventions.py'
-
 sed --in-place --regexp-extended "s@from cobaya import __version__@from cobaya.__init__ import __version__@g" 'conventions.py'
 sed --in-place --regexp-extended "s@from cobaya import __obsolete__@from cobaya.__init__ import __obsolete__@g" 'tools.py'
 
