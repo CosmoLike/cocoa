18bc262ed318db7d18bc714f4e1439dc9b5a20d5
Vivian Miranda <vivian.miranda@stonybrook.edu>
2024-06-24 17:12:04 -0400

    Update

--- Cocoa/installation_scripts/compile_core_packages.sh
+++ Cocoa/installation_scripts/compile_core_packages.sh
@@ -42,7 +42,7 @@ if [ -z "${IGNORE_CORE_INSTALLATION}" ]; then
   # ----------------------------------------------------------------------------
   # ----------------------------------------------------------------------------
 
-  ptop2 'COMPILE_CORE_C_PACKAGES' || return 1
+  ptop2 'COMPILE_CORE_PACKAGES' || return 1
 
   unset_env_vars || return 1; 
 
@@ -217,6 +217,7 @@ if [ -z "${IGNORE_CORE_INSTALLATION}" ]; then
   # ----------------------------------------------------------------------------
   # ---------------------------- LAPACK LIBRARY --------------------------------
   # ----------------------------------------------------------------------------
+  
   if [ -z "${IGNORE_FORTRAN_LAPACK_INSTALLATION}" ]; then
     
     ptop 'COMPILING LAPACK FORTRAN LIBRARY' || return 1;
@@ -250,7 +251,7 @@ if [ -z "${IGNORE_CORE_INSTALLATION}" ]; then
     pbottom 'COMPILING LAPACK FORTRAN LIBRARY' || return 1;
 
   fi
-
+  
   # ----------------------------------------------------------------------------
   # -------------------------------- FFTW --------------------------------------
   # ----------------------------------------------------------------------------
@@ -377,10 +378,139 @@ if [ -z "${IGNORE_CORE_INSTALLATION}" ]; then
   fi
 
   # ----------------------------------------------------------------------------
+  # ---------------------------------- SPDLOG ----------------------------------
+  # ----------------------------------------------------------------------------
+  
+  if [ -z "${IGNORE_CPP_SPDLOG_INSTALLATION}" ]; then
+    
+    ptop 'COMPILING SPDLOG CPP LIBRARY' || return 1
+
+    PACKDIR="${CCIL:?}/${COCOA_SPDLOG_DIR:-"spdlog/"}"
+
+    rm -f "${PACKDIR:?}/CMakeCache.txt"
+
+    cdfolder "${PACKDIR}" || return 1;
+    
+    ${CMAKE:?} -DCMAKE_INSTALL_PREFIX="${ROOTDIR:?}/.local" \
+      -DCMAKE_C_COMPILER="${C_COMPILER:?}" \
+      -DCMAKE_CXX_COMPILER="${CXX_COMPILER:?}" \
+      --log-level=ERROR . \
+      >${OUT1:?} 2>${OUT2:?} || { error "${EC12:?}"; return 1; }
+
+    make -j $MNT >${OUT1:?} 2>${OUT2:?} || { error "${EC8:?}"; return 1; }
+
+    make install >${OUT1:?} 2>${OUT2:?} || { error "${EC10:?}"; return 1; }
+
+    unset -v PACKDIR
+
+    cdfolder "${ROOTDIR}" || return 1;
+
+    pbottom 'COMPILING SPDLOG CPP LIBRARY' || return 1
+
+  fi
+
+  # ----------------------------------------------------------------------------
+  # ------------------------------ ARMADILLO -----------------------------------
+  # ----------------------------------------------------------------------------
+  
+  if [ -z "${IGNORE_CPP_ARMA_INSTALLATION}" ]; then
+    
+    ptop 'COMPILING ARMADILLO CPP LIBRARY' || return 1
+
+    PACKDIR="${CCIL:?}/${COCOA_ARMADILLO_DIR:-"armadillo-12.8.2/"}"
+
+    rm -f "${PACKDIR:?}/CMakeCache.txt"
+
+    cdfolder "${PACKDIR}" || return 1;
+
+    ${CMAKE:?} -DBUILD_SHARED_LIBS=TRUE \
+      -DCMAKE_INSTALL_PREFIX="${ROOTDIR:?}/.local" \
+      -DCMAKE_C_COMPILER="${C_COMPILER:?}" \
+      -DCMAKE_CXX_COMPILER="${CXX_COMPILER:?}" \
+      -DLAPACK_FOUND=YES \
+      -DLAPACK_LIBRARIES="${ROOTDIR:?}/.local/lib/liblapack.so" \
+      -DBLAS_FOUND=NO \
+      --log-level=ERROR . \
+      >${OUT1:?} 2>${OUT2:?} || { error "${EC12:?}"; return 1; }
+    
+    make clean >${OUT1:?} 2>${OUT2:?} || { error "${EC2:?}"; return 1; }
+
+    make -j $MNT all -Wno-dev \
+      >${OUT1:?} 2>${OUT2:?} || { error "${EC8:?}"; return 1; }
+
+    make install >${OUT1:?} 2>${OUT2:?} || { error "${EC10:?}"; return 1; }
+
+    unset -v PACKDIR
+
+    cdfolder "${ROOTDIR}" || return 1;
+
+    pbottom 'COMPILING ARMADILLO CPP LIBRARY' || return 1
+
+  fi
+
+  # ----------------------------------------------------------------------------
+  # ---------------------- CARMA (ARMADILLO <-> PYBIND11) ----------------------
+  # ----------------------------------------------------------------------------
+  
+  if [ -z "${IGNORE_CPP_CARMA_INSTALLATION}" ]; then
+    
+    ptop 'COMPILING CARMA CPP LIBRARY' || return 1
+
+    PACKDIR="${CCIL:?}/${COCOA_CARMA_DIR:-"carma/"}"
+
+    rm -rf "${ROOTDIR:?}/.local/include/carma/"   
+    
+    mkdir "${ROOTDIR:?}/.local/include/carma/" \
+      2>${OUT2:?} || { error "${EC20:?}"; return 1; }
+    
+    cpfile "${PACKDIR:?}/carma.h" "${ROOTDIR:?}/.local/include/" || return 1
+
+    cpfolder "${PACKDIR:?}/carma_bits" "${ROOTDIR:?}/.local/include/" || return 1
+
+    unset -v PACKDIR
+    
+    cdfolder "${ROOTDIR}" || return 1;
+
+    pbottom 'COMPILING CARMA CPP LIBRARY' || return 1
+
+  fi
+
+  # ----------------------------------------------------------------------------
+  # ---------------------------------- BOOST -----------------------------------
+  # ----------------------------------------------------------------------------
+  
+  if [ -z "${IGNORE_CPP_BOOST_INSTALLATION}" ]; then
+
+    ptop 'COMPILING BOOST CPP LIBRARY' || return 1
+
+    PACKDIR="${CCIL:?}/${COCOA_BOOST_DIR:-"boost_1_81_0/"}"
+
+    cdfolder "${PACKDIR}" || return 1;
+
+    ./bootstrap.sh --prefix="${ROOTDIR:?}/.local" \
+      >${OUT1:?} 2>${OUT2:?} || { error "${EC19:?}"; return 1; }
+
+    ./b2 --with=regex install \
+      --without-python \
+      --without-thread \
+      --without-timer  \
+      --without-mpi \
+      --without-atomic \
+      >${OUT1:?} 2>${OUT2:?} || { error "${EC21:?}"; return 1; }
+    
+    unset -v PACKDIR
+
+    cdfolder "${ROOTDIR}" || return 1;
+
+    pbottom 'COMPILING BOOST CPP LIBRARY' || return 1
+
+  fi
+
+  # ----------------------------------------------------------------------------
 
   unset_all || return 1; 
 
-  pbottom2 'COMPILE_CORE_C_PACKAGES DONE' || return 1
+  pbottom2 'COMPILE_CORE_PACKAGES' || return 1
 
 fi
 
