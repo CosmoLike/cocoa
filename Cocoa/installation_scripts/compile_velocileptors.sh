3997e59f995b9b5666be49f44836bff347875629
Vivian Miranda <vivian.miranda@stonybrook.edu>
2024-07-01 15:52:25 -0400

    Update

---
+++ Cocoa/installation_scripts/compile_velocileptors.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+if [ -z "${IGNORE_CAMB_COMPILATION}" ]; then
+  
+  if [ -z "${ROOTDIR}" ]; then
+    source start_cocoa.sh || { pfail 'ROOTDIR'; return 1; }
+  fi
+
+  # parenthesis = run in a subshell  
+  ( source "${ROOTDIR:?}/installation_scripts/flags_check.sh" ) || return 1;
+
+  unset_env_vars () {
+    unset -v ECODEF FOLDER PACKDIR
+    cdroot || return 1;
+  }
+
+  unset_env_funcs () {
+    unset -f cdfolder cpfolder error
+    unset -f unset_env_funcs
+    cdroot || return 1;
+  }
+
+  unset_all () {
+    unset_env_vars
+    unset_env_funcs
+    unset -f unset_all
+    cdroot || return 1;
+  }
+  
+  error () {
+    fail_script_msg "$(basename "${BASH_SOURCE[0]}")" "${1}"
+    unset_all || return 1
+  }
+  
+  cdfolder() {
+    cd "${1:?}" 2>"/dev/null" || { error "CD FOLDER ${1}"; return 1; }
+  }
+  
+  # --------------------------------------------------------------------------- 
+  # --------------------------------------------------------------------------- 
+  # ---------------------------------------------------------------------------
+
+  ptop 'COMPILING VELOCILEPTORS' || return 1
+
+  unset_env_vars || return 1
+
+  # E = EXTERNAL, CODE, F=FODLER
+  ECODEF="${ROOTDIR:?}/external_modules/code"
+
+  FOLDER=${VELOCILEPTORS_NAME:-"velocileptors"}
+
+  PACKDIR="${ECODEF:?}/${FOLDER:?}"
+
+  cdfolder "${PACKDIR}" || return 1
+
+  # ---------------------------------------------------------------------------
+  # cleaning any previous compilation
+  
+  rm -rf "${PACKDIR:?}/build"
+
+  "${PYTHON3:?}" setup.py clean \
+    >${OUT1:?} 2>${OUT2:?} || { error "${EC1:?}"; return 1; }
+  
+  # ---------------------------------------------------------------------------
+  
+  "${PYTHON3:?}" setup.py build \
+    >${OUT1:?} 2>${OUT2:?} || { error "${EC4:?}"; return 1; }
+
+  "${PYTHON3:?}" setup.py install \
+    >${OUT1:?} 2>${OUT2:?} || { error "${EC4:?}"; return 1; }
+
+  unset_all || return 1
+  
+  pbottom 'COMPILING VELOCILEPTORS' || return 1
+  
+fi
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
