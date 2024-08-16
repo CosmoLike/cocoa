cc22cf4f82d4bbfa7e35bbfce2e06ae695bb53f6
Vivian <vivian.miranda.astro@gmail.com>
2024-08-16 17:39:58 -0400

    Update

--- Cocoa/installation_scripts/compile_core_packages.sh
+++ Cocoa/installation_scripts/compile_core_packages.sh
@@ -822,6 +822,7 @@ if [ -z "${IGNORE_CORE_INSTALLATION}" ]; then
         'mpmath==1.3.0' \
         'multiprocess==0.70.14' \
         'networkx==3.1' \
+        'notebook==7.1.0' \
         'numba==0.57.0' \
         'numpy==1.23.5'  \
         'numpydoc==1.5.0' \
@@ -898,11 +899,14 @@ if [ -z "${IGNORE_CORE_INSTALLATION}" ]; then
 
     #PS: --force-reinstall - this helps CARMA to see numpy files
     env CXX="${CXX_COMPILER:?}" CC="${C_COMPILER:?}" ${PIP3:?} install \
+        'notebook==7.1.0' \
         'numpy==1.23.5' \
       --prefix="${ROOTDIR:?}/.local" \
       --force-reinstall \
       >${OUT1:?} 2>${OUT2:?} || { error "(CORE-PACKAGES) ${EC13:?}"; return 1; }
     
+    
+
     pbottom "INSTALLING PYTHON CORE LIBRARIES VIA PIP" || return 1
   
   fi
