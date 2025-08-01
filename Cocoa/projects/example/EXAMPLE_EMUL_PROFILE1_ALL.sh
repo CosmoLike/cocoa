5ab0e212f059bba22132d5f4b55273bf7d566b25
Vivian Miranda <vivian.miranda.astro@gmail.com>
2025-08-01 16:24:33 +0000

    Update

---
+++ Cocoa/projects/example/EXAMPLE_EMUL_PROFILE1_ALL.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#SBATCH --job-name=PROF1
+#SBATCH --output=PROF1-%A.out
+#SBATCH --nodes=1
+#SBATCH --ntasks=96
+#SBATCH --ntasks-per-node=94
+#SBATCH --ntasks-per-socket=47
+#SBATCH --cpus-per-task=1
+#SBATCH --time=48:00:00
+#SBATCH --partition=high_priority
+#SBATCH --qos=user_qos_timeifler
+#SBATCH --account=timeifler
+#SBATCH --exclusive
+
+echo Running on host `hostname`
+echo Time is `date`
+echo Directory is `pwd`
+echo Slurm job NAME is $SLURM_JOB_NAME
+echo Slurm job ID is $SLURM_JOBID
+echo Number of task is $SLURM_NTASKS
+echo Number of cpus per task is $SLURM_CPUS_PER_TASK
+
+cd $SLURM_SUBMIT_DIR
+conda activate cocoa
+source start_cocoa.sh
+
+export OMP_PROC_BIND=close
+if [ -n "$SLURM_CPUS_PER_TASK" ]; then
+  export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+else
+  export OMP_NUM_THREADS=1
+fi
+
+ulimit -u 2000000 # require line when nmpi is high
+for i in {0..6..1}
+do
+  mpirun -n 13 --oversubscribe --mca pml ^ucx --mca btl vader,tcp,self \
+       --bind-to core --map-by core --report-bindings --mca mpi_yield_when_idle 1 \
+      python ./projects/example/EXAMPLE_EMUL_PROFILE1.py \
+      --root ./projects/example/ --cov 'chains/EXAMPLE_EMUL_MCMC1.covmat' \
+      --outroot "EXAMPLE_EMUL_PROFILE1" --factor 3 --maxfeval 25000 --numpts 10 \
+      --profile $i --minfile="./projects/example/chains/EXAMPLE_EMUL_MIN1.txt" &
+done
