d08670d76f015c2b83af268bc8ea2f91c1bf4422
Vivian Miranda <vivian.miranda.astro@gmail.com>
2025-08-02 19:45:08 +0000

    Update

--- Cocoa/projects/example/EXAMPLE_EMUL_MINIMIZE1.py
+++ Cocoa/projects/example/EXAMPLE_EMUL_MINIMIZE1.py
@@ -69,8 +69,8 @@ likelihood:
     clik_file: plc_3.0/hi_l/plik_lite/plik_lite_v22_TTTEEE.clik
   planck_2018_lowl.TT: 
     path: ./external_modules
-  planck_2018_lowl.EE:
-    path: ./external_modules
+  planck_2020_lollipop.lowlE:
+    data_folder: planck/lollipop
   sn.desy5: 
     path: ./external_modules/data/sn_data
   bao.desi_dr2.desi_bao_all:
