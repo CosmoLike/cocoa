d349a939133dcdbcbb0a90e35393b41120ac3aa4
Vivian Miranda <vivian.miranda.astro@gmail.com>
2025-08-03 21:11:30 +0000

    Update

--- Cocoa/projects/example/scripts/EXAMPLE_PLOT_PROFILE1_COMP.py
+++ Cocoa/projects/example/scripts/EXAMPLE_PLOT_PROFILE1_COMP.py
@@ -43,13 +43,15 @@ bottom = gridspec.GridSpecFromSubplotSpec(1,
                                           wspace=0.35)
 ax += [fig.add_subplot(bottom[0,i]) for i in range(3)]
 # ------------------------------------------------------------------------------
-root = os.environ['ROOTDIR'] + "/projects/example/chains/EXAMPLE_EMUL_PROFILE1."
+root   = os.environ['ROOTDIR'] + "/projects/example/chains/EXAMPLE_EMUL_PROFILE1."
+rootM2 = os.environ['ROOTDIR'] + "/projects/example/chains/EXAMPLE_EMUL_PROFILE1M2."
 params = ['logA', 'ns', 'omegabh2', 'omegach2', 'thetastar', 'tau', 'A_planck' ]
 latex  = ["$\\log(10^{10} A_\\mathrm{s})$", "$n_\\mathrm{s}$", 
           "$100\\Omega_\\mathrm{b} h^2$", "$10\\Omega_\\mathrm{c} h^2$", 
           "$100\\theta_*$", "$\\tau_\\mathrm{reio}$", "$A_{\\rm Planck}$" ]
 # ------------------------------------------------------------------------------
 for i in range(7):
+# ------------------------------------------------------------------------------
     data = np.loadtxt(root + params[i] + '.txt', comments="#",)
     if i == 2:
         data[:,0] = 100*data[:,0]
@@ -59,15 +61,49 @@ for i in range(7):
     y  = data[:, 1]-np.min(data[:,1])
 
     ax[i].plot(x, y, 
-               marker='D',c='black', linestyle='None', markersize=4,
+               marker='D',c='blue', linestyle='None', markersize=4,
                alpha=1.0,lw=1.0,
-               label=params[i])
+               label=None)
     
     # fit a parabola
     coeffs = np.polyfit(x, y, deg=2)
     xfit = np.linspace(np.min(x), np.max(x), 300)
     yfit = np.polyval(coeffs, xfit)
-    ax[i].plot(xfit, yfit, color='blue', lw=1.5, alpha=0.7, label='Parabola fit')
+    ax[i].plot(xfit, 
+               yfit,
+               linestyle='solid', 
+               color='blue', 
+               lw=1.0, 
+               alpha=0.8, 
+               label='Procoli')
+# ------------------------------------------------------------------------------
+    data = np.loadtxt(rootM2 + params[i] + '.txt', comments="#",)
+    if i == 2:
+        data[:,0] = 100*data[:,0]
+    if i == 3:
+        data[:,0] = 10*data[:,0]
+    x  = data[:, 0]
+    y  = data[:, 1]-np.min(data[:,1])
+
+    ax[i].plot(x, y, 
+               marker='v',c='red', linestyle='None', markersize=4,
+               alpha=1.0,lw=1.0,
+               label=None)
+    
+    # fit a parabola
+    coeffs = np.polyfit(x, y, deg=2)
+    xfit = np.linspace(np.min(x), np.max(x), 300)
+    yfit = np.polyval(coeffs, xfit)
+    ax[i].plot(xfit, 
+               yfit, 
+               color='red', 
+               linestyle=':', 
+               lw=3.0, 
+               alpha=0.8,
+               label='SciPy (Nelder-Mead)')
+# ------------------------------------------------------------------------------
+    if i==4:
+        ax[i].legend(fontsize=12, frameon=False)
 # ------------------------------------------------------------------------------
     ax[i].grid(True)
     ax[i].grid(True, 
@@ -89,40 +125,6 @@ for i in range(7):
     ax[i].set_ylim(np.min(y),np.max(y))
     ax[i].set_xlim(data[0,0]-0.075*(data[-1,0]-data[0,0]),
                    x[-1]+0.075*(x[-1]-x[0]))
-# ------------------------------------------------------------------------------    
-    # get 1σ, 2σ and 3σ - print as vertical lines
-    sigma_lines = {}
-    for y0 in [1, 4, 9]:
-        a, b, c = coeffs
-        roots = np.roots([a, b, c - y0])
-        real_roots = [np.real(r) for r in roots if np.isreal(r)]
-        if len(real_roots) == 2:
-            sigma_lines[y0] = sorted(real_roots)
-            for r in real_roots:
-                ax[i].axvline(x=r, 
-                              linestyle='--', 
-                              color='grey', 
-                              alpha=0.5, 
-                              lw=1.0)
-    # Build annotation text
-    lmap = {1: "1σ", 4: "2σ", 9: "3σ"}
-    tlines = []
-    prec = max(0, int(-math.floor(math.log10(x[-1] - x[0]))) + 1)
-    for y0 in [1, 4, 9]:
-        if y0 in sigma_lines:
-            lo, hi = sigma_lines[y0]
-            tlines.append(f"{lmap[y0]}: [{lo:.{prec}f}, {hi:.{prec}f}]")    
-    ax[i].text(
-        0.5, 
-        0.90, 
-        "\n".join(tlines),
-        transform=ax[i].transAxes,
-        fontsize=11,
-        va='top', ha='center',
-        bbox=dict(boxstyle="round", 
-                  facecolor='white', 
-                  alpha=0.8, 
-                  edgecolor='black'))
 # ------------------------------------------------------------------------------
 plt.subplots_adjust(bottom=0.25, left = 0.2)
-plt.savefig(os.environ['ROOTDIR'] + "/projects/example/chains/EXAMPLE_PLOT_PROFILE1")
+plt.savefig(os.environ['ROOTDIR'] + "/projects/example/chains/EXAMPLE_PLOT_PROFILE1_COMP")
