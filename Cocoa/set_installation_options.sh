cd7c5292cc4f9146531eece59c3a8ef923fc2052
Vivian Miranda <vivian.miranda.astro@gmail.com>
2025-10-09 19:27:45 -0400

    Update

--- Cocoa/set_installation_options.sh
+++ Cocoa/set_installation_options.sh
@@ -51,7 +51,7 @@ export IGNORE_COSMOPOWER_DATA=1
 # The keys below control which packages will be installed and compiled 
 # ------------------------------------------------------------------------------
 #export IGNORE_CAMB_CODE=1
-export IGNORE_CLASS_CODE=1 # Default: we just use CAMB (reduces compilation time)
+#export IGNORE_CLASS_CODE=1 # Default: we just use CAMB (reduces compilation time)
 #export IGNORE_COSMOLIKE_CODE=1
 #export IGNORE_POLYCHORD_SAMPLER_CODE=1
 #export IGNORE_PLANCK_LIKELIHOOD_CODE=1
@@ -60,7 +60,7 @@ export IGNORE_CLASS_CODE=1 # Default: we just use CAMB (reduces compilation time
 export IGNORE_CPP_CUBA_INSTALLATION=1
 export IGNORE_FGSPECTRA_CODE=1
 export IGNORE_VELOCILEPTORS_CODE=1
-export IGNORE_SIMONS_OBSERVATORY_LIKELIHOOD_CODE=1
+#export IGNORE_SIMONS_OBSERVATORY_LIKELIHOOD_CODE=1
 export IGNORE_CAMSPEC_LIKELIHOOD_CODE=1
 export IGNORE_LIPOP_LIKELIHOOD_CODE=1
 export IGNORE_HYREC_CODE=1
@@ -82,8 +82,9 @@ export IGNORE_DARK_EMULATOR_CODE=1
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 #export IGNORE_COSMOLIKE_LSST_Y1_CODE=1
-export IGNORE_COSMOLIKE_DES_Y3_CODE=1
-export IGNORE_COSMOLIKE_ROMAN_FOURIER_CODE=1
+#export IGNORE_COSMOLIKE_DES_Y3_CODE=1
+#export IGNORE_COSMOLIKE_DESXPLANCK_CODE=1
+#export IGNORE_COSMOLIKE_ROMAN_FOURIER_CODE=1
 #export IGNORE_COSMOLIKE_ROMAN_REAL_CODE=1
 
 # ------------------------------------------------------------------------------
@@ -374,6 +375,11 @@ export ROMAN_REAL_BRANCH="main"
 #BRANCH: if unset, load the specified TAG
 #export ROMAN_REAL_TAG="v4.0-beta17"
 
+export DESXPLANCK_URL="https://git@github.com/CosmoLike/cocoa_desy1xplanck.git"
+export DESXPLANCK_NAME="desy1xplanck"
+export DESXPLANCK_BRANCH="main"
+#export DESXPLANCK_COMMIT=
+#export DESXPLANCK_TAG=
 # --------------------------------------------------------------------
 # --------------------------------------------------------------------
 # private projects below ---------------------------------------------
