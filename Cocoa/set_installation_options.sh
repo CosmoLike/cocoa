9c122a6429edae5e07f660cf5d58195acee4211f
Vivian Miranda <3210728+vivianmiranda@users.noreply.github.com>
2026-04-26 19:13:45 -0400

    Update set_installation_options.sh (#36)

--- Cocoa/set_installation_options.sh
+++ Cocoa/set_installation_options.sh
@@ -150,10 +150,10 @@ export ROMAN_REAL_NAME="roman_real"
 export ROMAN_REAL_GIT_TAG="v4.074"
 
 export DESXPLANCK_URL="https://git@github.com/CosmoLike/cocoa_desy1xplanck.git"
-#export DESXPLANCK_GIT_NAME="desy1xplanck"
+export DESXPLANCK_GIT_NAME="desy1xplanck"
 #export DESXPLANCK_GIT_BRANCH="main"
 #export DESXPLANCK_GIT_COMMIT=
-export DESXPLANCK_GIT_TAG=v4.07
+export DESXPLANCK_GIT_TAG="v4.071"
 
 export COSMOLIKE_URL="https://github.com/CosmoLike/cocoa-cosmolike-core.git"
 #export COSMOLIKE_GIT_COMMIT= ""
