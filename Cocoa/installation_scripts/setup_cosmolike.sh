58c63ace61699073284ffdf419b85364ae79b47d
Vivian Miranda <3210728+vivianmiranda@users.noreply.github.com>
2026-05-04 15:18:38 -0400

    Main2 (#46)
    - make sure branch name has a suffix TMP (when cloning based on TAG)

--- Cocoa/installation_scripts/setup_cosmolike.sh
+++ Cocoa/installation_scripts/setup_cosmolike.sh
@@ -96,7 +96,7 @@ if [ ! -d "${PACKDIR:?}" ]; then
     "${GIT:?}" checkout -b "${COSMOLIKE_GIT_BRANCH:?}" "origin/${COSMOLIKE_GIT_BRANCH:?}" \
       >>${OUT1:?} 2>>${OUT2:?} || { error "${EC16:?}"; return 1; }
   elif [ -n "${COSMOLIKE_GIT_TAG:-}" ]; then
-    "${GIT:?}" checkout "tags/${COSMOLIKE_GIT_TAG:?}" -b "${COSMOLIKE_GIT_TAG:?}" \
+    "${GIT:?}" checkout "tags/${COSMOLIKE_GIT_TAG:?}" -b "${COSMOLIKE_GIT_TAG:?}TMP" \
       >>${OUT1:?} 2>>${OUT2:?} || { error "${EC16:?}"; return 1; }
   fi
 
