d49ec278d7b22bc6823f71d049c2d60a9c5fa3d8
Vivian Miranda <3210728+vivianmiranda@users.noreply.github.com>
2026-05-12 17:38:47 -0400

    Updated flags (#74)

--- Cocoa/set_installation_options.sh
+++ Cocoa/set_installation_options.sh
@@ -110,23 +110,23 @@ export USE_SPT_CLIK_PLANCK=1
 
 export DES_Y3_URL="https://github.com/CosmoLike/cocoa_des_y3.git"
 export DES_Y3_NAME="des_y3"
-export DES_Y3_GIT_TAG="v4.10.2"
+export DES_Y3_GIT_TAG="v4.10.3"
 
 export DESXPLANCK_URL="https://git@github.com/CosmoLike/cocoa_desy1xplanck.git"
 export DESXPLANCK_GIT_NAME="desy1xplanck"
-export DESXPLANCK_GIT_TAG="v4.10.2"
+export DESXPLANCK_GIT_TAG="v4.10.3"
 
 export LSST_Y1_URL="https://github.com/CosmoLike/cocoa_lsst_y1.git"
 export LSST_Y1_NAME="lsst_y1"
-export LSST_Y1_GIT_TAG="v4.10.3"
+export LSST_Y1_GIT_TAG="v4.10.4"
 
 export ROMAN_FOURIER_URL="https://github.com/CosmoLike/cocoa_roman_fourier.git"
 export ROMAN_FOURIER_NAME="roman_fourier"
-export ROMAN_FOURIER_GIT_TAG="v4.10.2"
+export ROMAN_FOURIER_GIT_TAG="v4.10.3"
 
 export ROMAN_REAL_URL="https://github.com/CosmoLike/cocoa_roman_real.git"
 export ROMAN_REAL_NAME="roman_real"
-export ROMAN_REAL_GIT_TAG="v4.10.4"
+export ROMAN_REAL_GIT_TAG="v4.10.5"
 
 export COSMOLIKE_URL="https://github.com/CosmoLike/cocoa-cosmolike-core.git"
 export COSMOLIKE_GIT_TAG="v4.11.1"
