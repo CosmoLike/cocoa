21532b8a134bd67b4d06c3f83b8bf73e7656a660
Vivian Miranda <vivian.miranda.astro@gmail.com>
2026-09-23 08:45:43 -0400

    Updated set_installation_options.sh

--- Cocoa/set_installation_options.sh
+++ Cocoa/set_installation_options.sh
@@ -124,31 +124,31 @@ export USE_SPT_CLIK_PLANCK=1
 
 export DES_Y3_URL="https://github.com/CosmoLike/cocoa_des_y3.git"
 export DES_Y3_NAME="des_y3"
-export DES_Y3_GIT_TAG="v4.11"
+export DES_Y3_GIT_TAG="v4.11.1"
 
 export DESXPLANCK_URL="https://git@github.com/CosmoLike/cocoa_desy1xplanck.git"
 export DESXPLANCK_GIT_NAME="desy1xplanck"
-export DESXPLANCK_GIT_TAG="v4.11"
+export DESXPLANCK_GIT_TAG="v4.11.1"
 
 export LSST_Y1_URL="https://github.com/CosmoLike/cocoa_lsst_y1.git"
 export LSST_Y1_NAME="lsst_y1"
-export LSST_Y1_GIT_TAG="v4.11.1"
+export LSST_Y1_GIT_TAG="v4.11.2"
 
 export ROMAN_FOURIER_URL="https://github.com/CosmoLike/cocoa_roman_fourier.git"
 export ROMAN_FOURIER_NAME="roman_fourier"
-export ROMAN_FOURIER_GIT_TAG="v4.11"
+export ROMAN_FOURIER_GIT_TAG="v4.11.1"
 
 export ROMAN_REAL_URL="https://github.com/CosmoLike/cocoa_roman_real.git"
 export ROMAN_REAL_NAME="roman_real"
-export ROMAN_REAL_GIT_TAG="v4.11.1"
+export ROMAN_REAL_GIT_TAG="v4.11.2"
 
 export COSMOLIKE_URL="https://github.com/CosmoLike/cocoa-cosmolike-core.git"
-export COSMOLIKE_GIT_TAG="v4.11.5"
+export COSMOLIKE_GIT_TAG="v4.11.6"
 export COSMOLIKE_NAME="cosmolike_core"
 
 export ROMAN_KL_URL="https://github.com/CosmoLike/cocoa_roman_kl.git"
 export ROMAN_KL_NAME="roman_kl"
-export ROMAN_KL_GIT_TAG="v4.11.3"
+export ROMAN_KL_GIT_TAG="v4.11.4"
 
 export BFMT_THEORY_URL="https://github.com/CosmoLike/cocoa_baryonic_feedback_models_theory.git"
 export BFMT_NAME="baryon_suppression"
@@ -198,7 +198,7 @@ export FASTPT_NAME="FAST-PT"
 
 export FASTPT_WRAPPER_URL="https://github.com/CosmoLike/fastpt.git"
 export FASTPT_WRAPPER_NAME="PyFAST-PT"
-export FASTPT_WRAPPER_GIT_TAG="v1.0"
+export FASTPT_WRAPPER_GIT_TAG="v1.01"
 
 # ------------------------------------------------------------------------------
 # Build modes — select ONE via environment or make invocation:
